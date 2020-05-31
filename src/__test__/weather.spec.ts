3d71bb743d5ef3b8cc84614106e399a38f9f2b0f
victorjambo <victorjambo@live.com>
2020-05-31 18:15:51 +0300

    chore: add more tests

--- src/__test__/weather.spec.ts
+++ src/__test__/weather.spec.ts
@@ -46,4 +46,56 @@ describe('weather', () => {
 
     expect(weatherInfo).toEqual(mockNoLocation)
   })
+
+  it('should handleError', () => {
+    const consoleSpy = jest.spyOn(console, 'log')
+
+    const weather = new Weather(locations)
+    weather.handleError(mockError)
+
+    expect(consoleSpy).toBeCalled()
+    expect(consoleSpy).toBeCalledTimes(1)
+    expect(consoleSpy).toHaveBeenCalledWith('\x1b[31m%s\x1b[0m', mockError.message)
+  })
+
+  it('should handleError with no location found', () => {
+    const consoleSpy = jest.spyOn(console, 'log')
+
+    const weather = new Weather(locations)
+    weather.handleError(mockError, 'dummy location')
+
+    expect(consoleSpy).toBeCalled()
+    expect(consoleSpy).toBeCalledTimes(2)
+    expect(consoleSpy).toHaveBeenCalledWith('\x1b[31m%s\x1b[0m', '"dummy location" city not found\n')
+  })
+
+  it('should fetch multiple locations from api', async () => {
+    stub.resolves(Promise.resolve({ data: mockResponseData }))
+
+    const multipleLocations = ['Nairobi', 'New york']
+
+    const expectedWeatherInfo = [
+      {
+        humidity: 68,
+        timezone: 10800,
+        clouds: 0,
+        location: 'Nairobi',
+        windSpeed: 2.73,
+        weather: 'clear sky',
+      },
+      {
+        humidity: 68,
+        timezone: 10800,
+        clouds: 0,
+        location: 'Nairobi',
+        windSpeed: 2.73,
+        weather: 'clear sky',
+      },
+    ]
+
+    const weather = new Weather(multipleLocations)
+    const weatherInfo = await weather.getWeatherInfo()
+
+    expect(weatherInfo).toEqual(expectedWeatherInfo)
+  })
 })
